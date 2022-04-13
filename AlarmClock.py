4db88ad0ec2c552fa147346bd187bbb4f3ecb61b
2shweet <76894642+2shweet@users.noreply.github.com>
2022-04-13 15:41:29 +1000

    Update AlarmClock.py
    Just Updated the alarm clock for PM AM

--- AlarmClock.py
+++ AlarmClock.py
@@ -1,26 +1,15 @@
 import tkinter as tk
 from tkinter import *
 import datetime
-from bs4 import BeautifulSoup
+import time
+from threading import *
+from turtle import width
 import pyttsx3
 import requests
-from time import strftime
-
+from bs4 import BeautifulSoup
 
 
 engine = pyttsx3.init()
-
-mainWindow = tk.Tk()
-mainWindow.title('ALARM CLOCK')
-
-# setting up the time
-def time():
-    todayTime = datetime.datetime.now().strftime('%I:%M:%S %p')
-    timeLabel.config(text=todayTime)
-    timeLabel.after(1000, time)
-timeLabel = tk.Label(mainWindow, font=('calibri', 40, 'bold'), background ='Green', foreground='white')
-timeLabel.grid(row=0, column=0)
-
 # Setting up speech for alarm clock
 def speak(audio):
     engine.setProperty('rate', 170)  # setting up new voice rate you can adjust i find 170 most human like
@@ -31,7 +20,6 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
     engine.stop()
-
 # Weather function
 def Weather():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'}
@@ -39,7 +27,6 @@ def Weather():
     soup = BeautifulSoup(url.content, 'html.parser')
     name = soup.find("div", {'class': "BdB Bds(d) Bdbc(#fff.12) Fz(1.2em) Py(2px) O(0) Pos(r) forecast-item Cur(p)"})
     speak(name)
-
 # Gets the Latest world News from yahoo
 def WorldNews():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'}
@@ -47,37 +34,84 @@ def WorldNews():
     soup = BeautifulSoup(url.content, 'html.parser')
     name = soup.find("div", {'id': "mrt-node-YDC-Stream"})
     speak(name)
-
-# setting up the alarm
-def Alarm():
-    alarmTime = f'{Hour.get()}:{minutes.get()}:{datetime.datetime.now().strftime("%S PM")}'
-    current_time = datetime.datetime.now().strftime("%I:%M:%S %p")
-    timeLabel.config(text=current_time)
-    timeLabel.after(1000, Alarm)
+# Creating the Alarm MainScreen 
+main_window = Tk()
+main_window.title('My Python Alarm Clock')
+# Set geometry
+main_window.geometry("300x150")
+def Time():
+    todayTime = datetime.datetime.now().strftime('%I:%M:%S %p')
+    timeLabel.config(text=todayTime)
+    timeLabel.after(1000, Time)
+timeLabel = tk.Label(main_window, font=('Roboto', 37, 'bold'), background ='#66ff00', foreground='#1900ff')
+timeLabel.pack(pady=5)
+Time()
+# Use Threading
+def Threading():
+    hrs.config(bg='#5a5a5a', font=('calibri 10 bold'), foreground='#94ff4d')
+    mins.config(bg='#5a5a5a', font=('calibri 10 bold'), foreground='#94ff4d')
+    secs.config(bg='#5a5a5a', font=('calibri 10 bold'), foreground='#94ff4d')
+    AM_PM.config(bg='#5a5a5a', font=('calibri 10 bold'), foreground='#94ff4d')
+    t1=Thread(target=alarm)
+    t1.start()
+def alarm():
     while True:
-        if alarmTime == current_time:
+        set_alarm_time = f"{hour.get()}:{minute.get()}:{second.get()} {am_pm.get()}" 
+        time.sleep(1)
+        # Getting the alarm and current time to set off the alarm 
+        current_time = datetime.datetime.now().strftime("%I:%M:%S %p")
+        print(current_time,set_alarm_time)
+        if current_time == set_alarm_time:
             speak(f'Good Morning The Current time is {current_time}')
             speak('The Weather Today is Looking')
             Weather()
             speak('In World News Today')
-            WorldNews()
-        if current_time >= alarmTime:
-            break
-        return SetAlarm
-
-# Creating the GUI for the alarm     
-Hour = tk.StringVar(mainWindow)
-minutes = tk.StringVar(mainWindow)
-
-# Setting Up the Hour for Alarm
-HourSet = tk.Label(mainWindow, text='HOURS').grid(row=1, column=0, sticky='nw', padx=20)
-HourSetEntry = tk.Entry(mainWindow, width=25, textvariable=Hour).grid(row=1, column=0, sticky='n')
-Hour.set(HourSetEntry)
-
-#Setting up the seconds for the alarm
-MinutesSet = tk.Label(mainWindow, text='MINUTES').grid(row=2, column=0, sticky='nw', padx=10)
-MinuteSetEntry= tk.Entry(mainWindow, width=25, textvariable=minutes).grid(row=2, column=0, sticky='n')
-minutes.set(MinuteSetEntry)
-SetAlarm = tk.Button(mainWindow, text="Set Alarm", command=Alarm).grid(row=4, column=0)
-time()
-mainWindow.mainloop()
+            WorldNews()     
+# Add Labels, Frame, Button, Option menus
+frame = Frame(main_window)
+frame.pack()
+# setting up the hours
+hour = StringVar(main_window)
+hours = ('01', '02', '03', '04', '05', '06', '07',
+         '08', '09', '10', '11', '12')
+hour.set(hours[0])
+hrs = OptionMenu(frame, hour, *hours)
+hrs.config(bg='#94ff4d', font=('calibri 10 bold'), foreground='#1900ff')
+hrs.pack(side=LEFT)
+# setting up the minutes 
+minute = StringVar(main_window)
+minutes = ('00', '01', '02', '03', '04', '05', '06', '07',
+           '08', '09', '10', '11', '12', '13', '14', '15',
+           '16', '17', '18', '19', '20', '21', '22', '23',
+           '24', '25', '26', '27', '28', '29', '30', '31',
+           '32', '33', '34', '35', '36', '37', '38', '39',
+           '40', '41', '42', '43', '44', '45', '46', '47',
+           '48', '49', '50', '51', '52', '53', '54', '55',
+           '56', '57', '58', '59', '60')
+minute.set(minutes[0])
+mins = OptionMenu(frame, minute, *minutes)
+mins.config(bg='#94ff4d', font=('calibri 10 bold'), foreground='#1900ff')
+mins.pack(side=LEFT)
+# setting up the seconds 
+second = StringVar(main_window)
+seconds = ('00', '01', '02', '03', '04', '05', '06', '07',
+           '08', '09', '10', '11', '12', '13', '14', '15',
+           '16', '17', '18', '19', '20', '21', '22', '23',
+           '24', '25', '26', '27', '28', '29', '30', '31',
+           '32', '33', '34', '35', '36', '37', '38', '39',
+           '40', '41', '42', '43', '44', '45', '46', '47',
+           '48', '49', '50', '51', '52', '53', '54', '55',
+           '56', '57', '58', '59', '60')
+second.set(seconds[0])
+secs = OptionMenu(frame, second, *seconds)
+secs.config(bg='#94ff4d', font=('calibri 10 bold'), foreground='#1900ff')
+secs.pack(side=LEFT)
+# setting up the AM or PM
+am_pm = StringVar(main_window)
+am_Pm = ('AM', 'PM')
+am_pm.set(am_Pm[0])
+AM_PM = OptionMenu(frame, am_pm, *am_Pm)
+AM_PM.config(bg='#94ff4d', font=('calibri 10 bold'), foreground='#1900ff')
+AM_PM.pack(side=LEFT)
+Button(main_window,text="SET ALARM",font=("Roboto 15 bold"),command=Threading, background ='#94ff4d', foreground='#1900ff', width=300).pack(pady=5, padx=5) 
+main_window.mainloop()
